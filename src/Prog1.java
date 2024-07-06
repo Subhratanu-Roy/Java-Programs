706234b82789be31037f5683fb4e14ded5481240
Subhratanu <subhratanu15@gmail.com>
2024-07-06 14:23:15 +0530

    Commi from American guy

--- src/Prog1.java
+++ src/Prog1.java
@@ -4,17 +4,7 @@ public class Prog1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
-		System.out.println("Updated 1st time");
-		System.out.println("Updated 2nd time");
-		System.out.println("Updated 3rd time");
-		System.out.println("Updated 4th time");
-		
-		System.out.println("1st Change done in develop branch");
-		System.out.println("2nd change done in develop branch");
-		System.out.println("3rd change done in develop branch");
-		System.out.println("4th change done in develop branch");
-		System.out.println("5th change done in develop branch");
+		System.out.println("Simplicity is the ultimate sophistication ");
 	}
 
 }
---
+++ src/Prog2.java
@@ -0,0 +1,9 @@
+
+public class Prog2 {
+
+	public static void main(String ars[]) {
+		
+		System.out.println("World is beautiful");
+	}
+	
+}
